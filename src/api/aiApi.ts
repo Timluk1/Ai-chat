2bb120cfa7dded751a3ebb975d48e8a1c3255bef
Timluk1 <main345242@gmail.com>
2024-12-19 23:11:37 +0300

    I think env works. I change name env variable

--- src/api/aiApi.ts
+++ src/api/aiApi.ts
@@ -5,9 +5,8 @@ class AiApi {
     private genAi: GoogleGenerativeAI;
     private model: GenerativeModel;
     constructor() {
-        console.log("KEY API", import.meta.env.VITE_GOOGLE_API_KEY)
         this.genAi = new GoogleGenerativeAI(
-            import.meta.env.VITE_GOOGLE_API_KEY,
+            import.meta.env.VITE_API_URL,
         );
         this.model = this.genAi.getGenerativeModel({
             model: "gemini-1.5-flash",
